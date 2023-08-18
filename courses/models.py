7d3a1457fdcac7019d81495733eeaf02f3c4714e
lyxlwz <1850115720@qq.com>
2023-08-18 09:42:23 +0800

    🎉 定义模型

--- courses/models.py
+++ courses/models.py
@@ -144,3 +144,114 @@ class AddressInfo(models.Model):  # couser_addressInfo
         # app_label = 'courses' # 这个等于 setting.py 里面 INATALLS_APP
 
         # db_tablespace  #定义数据库表空间的名字
+
+
+class Teacher(models.Model):
+    """
+    讲师信息表
+    """
+
+    name = models.CharField(
+        max_length=30, primary_key=True, db_index=True, verbose_name="昵称"
+    )
+    introduction = models.TextField(default="这位同学很懒，木有签名的说~", verbose_name="简介")
+    fans = models.PositiveBigIntegerField(default="0", verbose_name="粉丝数")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        verbose_name = "讲师信息表"  # 模型类设置可读信息
+        verbose_name_plural = verbose_name  # 因为英文所有有复数
+
+    # 给模型对象返回了讲师的昵称
+    def __str__(self):  # Python2:__unicode__
+        return super().nickname
+
+
+class Course(models.Model):
+    """
+    课程信息表
+    """
+
+    title = models.CharField(
+        max_length=100, primary_key=True, db_index=True, verbose_name="课程名"
+    )
+    """
+    choices 对应数据库的枚举类型
+
+    一般情况下，对于一些可选且有限的字段，可以使用枚举类型
+
+    一来可以t提高查询效率，二来也可以节省空间
+    """
+    type = models.CharField(
+        choices=((1, "实战课"), (2, "免费课"), (0, "其他")),
+        max_length=1,
+        default=0,
+        verbose_name="课程类型",
+    )
+    price = models.PositiveSmallIntegerField(verbose_name="价格")
+    volume = models.BigIntegerField(verbose_name="销量")
+    online = models.DateField(verbose_name="上线时间")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        verbose_name = "课程信息表"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        """
+        get_type_display() 表示获取type选择的值的显示
+
+        get_ + 字段名 + display()
+        """
+        # return f"{self.get_type_display()}-{self.title}"  # 实例：实战课Django 零基础入门到实战
+        return "{}-{}".format(self.get_type_display(), self.title)  # 另一种写法
+
+
+class Student(models.Model):
+    """
+    学生信息表
+    """
+
+    nickname = models.CharField(
+        max_length=30, primary_key=True, db_index=True, verbose_name="昵称"
+    )
+    age = models.PositiveSmallIntegerField(verbose_name="年龄")
+    gender = models.CharField(
+        choices=((1, "男"), (2, "女"), (0, "保密")),
+        max_length=1,
+        default=0,
+        verbose_name="性别",
+    )
+    study_time = models.PositiveIntegerField(default="0", verbose_name="学习时长(h)")
+    created_at = models.DateField(auto_now_add=True, verbose_name="创建时间")
+    update_at = models.DateField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        verbose_name = "学生信息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.nickname
+
+
+class TeacherAssistant(models.Model):
+    """
+    助教信息表
+    """
+
+    nickname = models.CharField(
+        max_length=30, primary_key=True, db_index=True, verbose_name="昵称"
+    )
+    bobby = models.CharField(max_length=100, null=True, blank=True, verbose_name="爱好")
+    created_at = models.DateField(auto_now_add=True, verbose_name="创建时间")
+    update_at = models.DateField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        verbose_name = "助教信息"
+        db_table = "courses_assistant"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.nickname
