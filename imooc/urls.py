13c9d5f8191f0e2ecb61544a1e0eeaaad1fa7076
lyxlwz <1850115720@qq.com>
2023-08-24 11:09:27 +0800

    🎉 补充

--- imooc/urls.py
+++ imooc/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from courses.views import IndexView, AddressAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", IndexView.as_view(), name="index"),
     path("address/<int:address_id>", AddressAPIView.as_view(), name="address"),
+    path("api-anth/", include("rest_framework.urls")),  # DRF的登陆退出
 ]
